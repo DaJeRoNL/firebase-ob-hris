e18f24df844143845bf4b339e4c1ef96bc7c2a58
DaJeRoNL <102187272+DaJeRoNL@users.noreply.github.com>
2025-12-10 22:09:02 +0100

    -

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,8 +2,8 @@
 export default {
   darkMode: 'class', // Enables manual toggling via the 'dark' class
   content: [
-    "./frontend/index.html",
-    "./frontend/src/**/*.{js,ts,jsx,tsx}",
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
